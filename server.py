4e416234f4dbd713aee46e03c7bb0c889b6ffe95
Shadow750amr <rmarcoa098@gmail.com>
2026-05-26 03:26:52 -0400

    changes in server

--- server.py
+++ server.py
@@ -1,14 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/')
-def hola():
-    return {"text":"hola"}
-
-@app.route('/emotionDetector/')
 def template():
+    # html
     return render_template("index.html")
 
-if __name__=="__main__":
-    app.run(debug=True,port=5000)
+@app.route('/emotionDetector', methods=['GET'])
+def return_data():
+    # captura de l oque manda js
+    texto_usuario = request.args.get('textToAnalyze')
+    resultado = emotion_detector(texto_usuario)
+    resultado_sin_emotion = resultado.pop('dominant_emotion')
+    return f"For the given statement, the system response is {resultado}." f"The dominant emotion is {resultado_sin_emotion}"
+if __name__ == "__main__":
+    app.run(debug=True, port=5000) 
